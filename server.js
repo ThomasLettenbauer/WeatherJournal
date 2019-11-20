6ba4a13ac3b4cbe90b6fa9a7b9e7e06e6f7b6684
BuildTools <unconfigured@null.spigotmc.org>
2019-11-21 00:34:22 +0100

    Init

--- server.js
+++ server.js
@@ -1,22 +1,72 @@
 // Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
 // Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Dependencies */
+const bodyParser = require('body-parser')
+const cors = require('cors');
+
 /* Middleware*/
 
 //Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
+app.use(express.static('website'));
 
 // Spin up the server
+const port = 3000;
+const server = app.listen(port, listening);
+
 // Callback to debug
+function listening() {
+    // console.log(server);
+    console.log(`running on localhost: ${port}`);
+};
 
 // Initialize all route with a callback function
+app.get('/all', sendData);
 
 // Callback function to complete GET '/all'
+function sendData(request, response) {
+    
+    console.log('GET')
+    console.log(projectData)
 
-// Post Route
-  
+    response.send(projectData);
+};
+
+// Post Route - Weather data
+app.post('/addWeatherData', addWeatherData);
+
+function addWeatherData(req, res) {
+
+    newEntry = {
+        temperature: req.body.temperature,
+        date: req.body.date,
+        userResponse: req.body.userResponse
+    }
+
+    projectData = newEntry;
+
+    console.log('POST')
+    console.log(projectData)
+
+};
+
+
+
+
+
+
+
+
--- website/app.js
+++ website/app.js
@@ -1,12 +1,83 @@
 // Personal API Key for OpenWeatherMap API
+const apiKey = '&APPID=ff6a890de33795a2a130e0e77b402f46';
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?units=metric&zip=';
 
 // Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', performAction);
 
 /* Function called by event listener */
+function performAction(e) {
+    const zipCode = document.getElementById('zip').value;
+    const myFeelings = document.getElementById('feelings').value;
+
+    getWeatherData(baseURL, zipCode, apiKey).then(function (weatherData) {
+        postData('http://localhost:3000/addWeatherData', { temperature: weatherData.main.temp, date: weatherData.dt, userResponse: myFeelings })
+    }).then( function () {       updateUI('http://localhost:3000/all') }
+        ).then(function (value) {
+        console.log('all promises resolved');
+    }, function (reason) {
+        console.log('some promise was rejected');
+    }
+
+    )
+}
 
 /* Function to GET Web API Data*/
+const getWeatherData = async (baseURL, zipCode, apiKey) => {
+
+    console.log('WEATHER');
+
+    const res = await fetch(baseURL + zipCode + ',de' + apiKey)
+    try {
+        const data = await res.json();
+        console.log(data)
+        return data;
+    } catch (error) {
+        console.log("error", error);
+    }
+}
+
 
 /* Function to POST data */
+const postData = async (url = '', data = {}) => {
 
+    console.log('POST');
+    console.log(data);
 
-/* Function to GET Project Data */
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data), // body data type must match "Content-Type" header        
+    });
+
+    console.log(data)
+
+    try {
+        const newData = await response.json();
+        return newData;
+    } catch (error) {
+        console.log("error", error);
+    }
+};
+
+/* Function to GET Project Data and Update UI */
+const updateUI = async (url = '') => {
+
+    console.log('UPDATE');
+
+    const res = await fetch(url);
+    try {
+        const data = await res.json();
+        console.log(data)
+
+        document.getElementById("temp").innerHTML = data.temperature;
+        document.getElementById("date").innerHTML = data.date;
+        document.getElementById("content").innerHTML = data.userResponse;
+
+    } catch (error) {
+        console.log("error", error);
+    }
+}
